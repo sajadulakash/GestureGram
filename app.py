809318840ac8f9297c97a9eaae7ee8c7840cf84a
Azfar Shams <azfarshams1998@gmail.com>
2024-02-03 03:19:28 +0600

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,5 @@ def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Enable debug mode for easier development
+    #app.run(debug=True)  # Enable debug mode for easier development
+    app.run(host="0.0.0.0", port=8000)
